994a7a64d7bb42051975fb2665ee3b64a10c4cc5
DavisV06 <vivianajoan252@gmail.com>
2025-06-13 01:14:09 -0500

    Carga de página de registro. Aun falta agregar los puntos para adjuntar los creditos y constancia

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,13 @@
 <script>
   import { Router, Route } from 'svelte-navigator';
   import Login from './routes/Login.svelte';
+  import registro from './routes/registro.svelte'
 </script>
 
 <Router>
-  <Route path="/" component={Login} />
+  <!-- Ruta para la pantalla de Login -->
+  <Route path="/login" component={Login}></Route>
+
+  <!-- Ruta para la pantalla de Registro -->
+  <Route path="/registro" component={registro}></Route>
 </Router>
