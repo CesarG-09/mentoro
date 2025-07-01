70ea3c340feda3daad0445cd26cb1dc8352c7e24
natashadau <natasha.urdaneta@utp.ac.pa>
2025-06-30 19:35:05 -0500

    Pantallas de admin, cambios en tamaño pendientes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,11 +2,18 @@
   import { Router, Route } from 'svelte-navigator';
   import Login from './routes/Login.svelte';
   import EstudianteInicio from './routes/EstudianteInicio.svelte';
-  import Registro from './routes/Registro.svelte';
+  import registro from './routes/registro.svelte';
+
+  import admin_dash from './routes/admin_dash.svelte';
+  //import admin_estud1 from './routes/admin_estud1.svelte';
+  import admin_estud2 from './routes/admin_estud2.svelte';
 </script>
 
 <Router>
   <Route path="/login" component={Login} />
-  <Route path="/registro" component={Registro} />
+  <Route path="/registro" component={registro} />
   <Route path="/estudiante/inicio" component={EstudianteInicio} />
+
+  <Route path="/admin_dash" component={admin_dash} />
+  <Route path="/admin_estud2" component={admin_estud2} />
 </Router>
